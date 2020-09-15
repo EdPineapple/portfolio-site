b01261d18312486678b90fe3365be0f886cab3fa
Ed <46209008+EdPineapple@users.noreply.github.com>
2020-09-15 02:07:49 -0400

    updated navigation bar styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+/*
+
 function menuToggle() {
 	var x = document.getElementById('myNavtoggle');
 	if (x.className === 'navtoggle') {
@@ -7,3 +10,32 @@ function menuToggle() {
     x.className = 'navtoggle';
   	}
 }
+
+*/
+
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click',() => {
+
+		nav.classList.toggle('nav-active');
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			}
+			else {
+				link.style.animation = `navLinkFade 1s ease forwards ${index / 7}s`;
+			}
+		});
+
+		burger.classList.toggle('toggle');
+
+	});
+	
+}
+
+navSlide();
