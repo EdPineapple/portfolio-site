09c8d63554fe86544ba310f04756890ab809339a
Ed <46209008+EdPineapple@users.noreply.github.com>
2020-09-15 16:03:27 -0400

    updated fade-in animations and added scroll-to-top button on vela.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
 
-/*
-
-function menuToggle() {
-	var x = document.getElementById('myNavtoggle');
-	if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  	} 
-  	else {
-    x.className = 'navtoggle';
-  	}
-}
-
-*/
+/* =============================== [NAV BAR] ==================================== */
 
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
-
 	const navLinks = document.querySelectorAll('.nav-links li');
-
 	burger.addEventListener('click',() => {
-
 		nav.classList.toggle('nav-active');
-
 		navLinks.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = '';
@@ -31,11 +15,21 @@ const navSlide = () => {
 				link.style.animation = `navLinkFade 1s ease forwards ${index / 7}s`;
 			}
 		});
-
 		burger.classList.toggle('toggle');
-
 	});
-	
 }
-
 navSlide();
+
+/* ======================= [SCROLL TO TOP BUTTON] ============================ */
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 1500) {
+        $('#btnScrollToTop').fadeIn(400);
+    } else { // else
+        $('#btnScrollToTop').fadeOut(400);
+    }
+});
+
+$('#btnScrollToTop').click(function() {
+    $('body,html').animate({scrollTop:0},200);
+});
